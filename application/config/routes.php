29da8f2c7c3b3c3218e4da4cd5b54bba32125860
arkans <82564511+arkansys22@users.noreply.github.com>
2025-01-13 21:34:29 +0700

    up

---
+++ application/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$route['default_controller'] = 'welcome';
+$route['404_override'] = 'Notfound';
+$route['translate_uri_dashes'] = FALSE;
+
+$route['login'] = "User/login";
+
+$route['petacrawl\.xml'] = "petacrawl";
