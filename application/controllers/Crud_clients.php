0ccc2b061fc34e7edb7d35132b35d1a793aa6da2
galihnkk <galihnkk96@gmail.com>
2025-02-10 14:45:50 +0700

    add clients, controller, model, index etc

---
+++ application/controllers/Crud_clients.php
@@ -0,0 +1,50 @@
+<?php
+class Crud_clients extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Clients_model'); // Ubah dari Client_model ke Clients_model
+        $this->load->helper('url');
+    }
+
+    public function index() {
+        $data['clients'] = $this->Clients_model->get_all_clients(); // Ubah pemanggilan model
+        $this->load->view('clients/index', $data);
+    }
+
+    public function create() {
+        $this->load->view('clients/create');
+    }
+
+    public function store() {
+        $data = [
+            'id_session' => uniqid(),
+            'name' => $this->input->post('name'),
+            'email' => $this->input->post('email'),
+            'phone' => $this->input->post('phone'),
+            'wedding_date' => $this->input->post('wedding_date'),
+        ];
+        $this->Clients_model->insert_client($data); // Ubah pemanggilan model
+        redirect('clients');
+    }
+
+    public function edit($id_session) {
+        $data['client'] = $this->Clients_model->get_client_by_session($id_session); // Ubah pemanggilan model
+        $this->load->view('clients/edit', $data);
+    }
+
+    public function update($id_session) {
+        $data = [
+            'name' => $this->input->post('name'),
+            'email' => $this->input->post('email'),
+            'phone' => $this->input->post('phone'),
+            'wedding_date' => $this->input->post('wedding_date'),
+        ];
+        $this->Clients_model->update_client($id_session, $data); // Ubah pemanggilan model
+        redirect('clients');
+    }
+
+    public function delete($id_session) {
+        $this->Clients_model->delete_client($id_session); // Ubah pemanggilan model
+        redirect('clients');
+    }
+}
---
+++ application/models/Clients_model.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Clients_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get_all_clients() {
+        return $this->db->get('clients')->result();
+    }
+
+    public function insert_client($data) {
+        return $this->db->insert('clients', $data);
+    }
+
+    public function get_client_by_session($id_session) {
+        return $this->db->get_where('clients', ['id_session' => $id_session])->row();
+    }
+
+    public function update_client($id_session, $data) {
+        $this->db->where('id_session', $id_session);
+        return $this->db->update('clients', $data);
+    }
+
+    public function delete_client($id_session) {
+        $this->db->where('id_session', $id_session);
+        return $this->db->delete('clients');
+    }
+}
