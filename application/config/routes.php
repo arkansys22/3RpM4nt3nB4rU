1b545266c5d0d74e511edb1e536ad761ed08ebdb
galihnkk <galihnkk96@gmail.com>
2025-02-10 17:17:01 +0700

    remove non needed routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -10,7 +10,6 @@ $route['panel'] = "aspanel/home";
 
 $route['projek'] = 'crud_projek/index'; // Menampilkan daftar projek
 $route['projek/create'] = 'crud_projek/create'; // Menampilkan form tambah projek
-$route['projek/create/submit'] = 'crud_projek/create'; // Menangani submit form tambah projek
 $route['projek/edit/(:any)'] = 'crud_projek/edit/$1'; // Menampilkan form edit projek
 $route['projek/update/(:any)'] = 'crud_projek/update/$1'; // Mengupdate projek
 $route['projek/delete/(:any)'] = 'crud_projek/delete/$1'; // Menghapus projek
@@ -18,7 +17,6 @@ $route['projek/recycle_bin'] = 'crud_projek/recycle_bin'; // Halaman recycle bin
 $route['projek/restore/(:any)'] = 'crud_projek/restore/$1'; // Restore klien
 $route['projek/permanent_delete/(:any)'] = 'crud_projek/permanent_delete/$1'; // Hapus permanen klien
 
-$route['default_controller'] = 'crud_clients'; // Menjadikan halaman klien sebagai default
 $route['clients'] = 'crud_clients/index'; // Menampilkan daftar klien
 $route['clients/create'] = 'crud_clients/create'; // Halaman tambah klien
 $route['clients/edit/(:any)'] = 'crud_clients/edit/$1'; // Halaman edit klien berdasarkan id_session
