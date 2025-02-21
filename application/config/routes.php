7c914a77d648946e26545cad88ded2b7afd86fc9
arkans <82564511+arkansys22@users.noreply.github.com>
2025-02-21 19:21:35 +0700

    up
    fitur user

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,6 +9,20 @@ $route['login'] = "User/login";
 $route['panel'] = "aspanel/home";
 $route['logout'] = "aspanel/logout";
 
+
+
+$route['user'] = 'crud_user/index'; // Menampilkan daftar projects
+$route['projects/create'] = 'crud_projects/create'; // Menampilkan form tambah projects
+$route['projects/store'] = 'crud_projects/store'; // Menyimpan data projects baru
+$route['projects/edit/(:any)'] = 'crud_projects/edit/$1'; // Menampilkan form edit projects
+$route['projects/update/(:any)'] = 'crud_projects/update/$1'; // Mengupdate projects
+$route['projects/delete/(:any)'] = 'crud_projects/delete/$1'; // Menghapus projects
+$route['projects/recycle_bin'] = 'crud_projects/recycle_bin'; // Halaman recycle bin
+$route['projects/restore/(:any)'] = 'crud_projects/restore/$1'; // Restore clients
+$route['projects/permanent_delete/(:any)'] = 'crud_projects/permanent_delete/$1'; // Hapus permanen clients
+
+
+
 $route['projects'] = 'crud_projects/index'; // Menampilkan daftar projects
 $route['projects/create'] = 'crud_projects/create'; // Menampilkan form tambah projects
 $route['projects/store'] = 'crud_projects/store'; // Menyimpan data projects baru
@@ -31,23 +45,14 @@ $route['clients/permanent_delete/(:any)'] = 'crud_clients/permanent_delete/$1'; 
 
 
 $route['potensial-clients'] = 'crud_potensial_clients/index'; // Menampilkan daftar Potensial Clients
-
 $route['potensial-clients-hot'] = 'crud_potensial_clients/index_hot'; // Menampilkan daftar Potensial Clients Hot
-
 $route['potensial-clients-konsul'] = 'crud_potensial_clients/index_konsul'; // Menampilkan daftar Potensial Clients konsul
-
 $route['potensial-clients-bayar'] = 'crud_potensial_clients/index_deal'; // Menampilkan daftar Potensial Clients bayar
-
 $route['potensial-clients-batal'] = 'crud_potensial_clients/index_batal'; // Menampilkan daftar Potensial Clients batal
-
 $route['potensial-clients-ghosting'] = 'crud_potensial_clients/index_ghosting'; // Menampilkan daftar Potensial Clients Ghosting
-
 $route['potensial-clients/create'] = 'crud_potensial_clients/create'; // Menampilkan form tambah Potensial Clients
 $route['potensial-clients/store'] = 'crud_potensial_clients/store'; // Menyimpan data Potensial Clients baru
-
 $route['potensial-clients/lihat/(:any)'] = 'crud_potensial_clients/lihat/$1'; // Menampilkan lihat Potensial Clients
-
-
 $route['potensial-clients/edit/(:any)'] = 'crud_potensial_clients/edit/$1'; // Menampilkan form edit Potensial Clients
 $route['potensial-clients/update/(:any)'] = 'crud_potensial_clients/update/$1'; // Mengupdate Potensial Clients
 $route['potensial-clients/delete/(:any)'] = 'crud_potensial_clients/delete/$1'; // Menghapus Potensial Clients
---
+++ application/controllers/Crud_user.php
@@ -0,0 +1,252 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class crud_user extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Potensial_model');
+        $this->load->helper('url');
+    }
+
+    public function index() {
+        if ($this->session->level=='1' OR $this->session->level=='2' OR $this->session->level=='3' OR $this->session->level=='4' OR $this->session->level=='5'){
+                cek_session_akses('home',$this->session->id_session);
+        $data['potensial_clients'] = $this->Potensial_model->get_all_potensial_clients(); // Ubah pemanggilan model
+        $this->load->view('user/index', $data);
+        }else{
+                redirect(base_url());
+                }
+    }
+
+
+    public function create() {
+        if ($this->session->level=='1' OR $this->session->level=='2' OR $this->session->level=='3' OR $this->session->level=='4' OR $this->session->level=='5'){
+            cek_session_akses('potensial-clients',$this->session->id_session);
+        $this->load->view('potensial_clients/create');
+        }else{
+                redirect(base_url());
+                }
+    }
+
+    public function store() {
+        $id_session2 = sha1(uniqid());
+        $date_create = date('Y-m-d H:i:s');  // tanggal dan waktu
+        
+        if ($this->agent->is_browser()) // Agent untuk fitur di log activity
+                {
+                      $agent = 'Desktop ' .$this->agent->browser().' '.$this->agent->version();
+                }
+                elseif ($this->agent->is_robot())
+                {
+                      $agent = $this->agent->robot();
+                }
+                elseif ($this->agent->is_mobile())
+                {
+                      $agent = 'Mobile' .$this->agent->mobile().''.$this->agent->version();
+                }
+                else
+                {
+                      $agent = 'Unidentified User Agent';
+                }
+
+        $data = array(
+            'id_session'    => $id_session2,
+            'pc_name'  => $this->input->post('pc_name'),
+            'pc_nowa'  => $this->input->post('pc_nowa'),
+            'status'        => 'Tanya-tanya',
+            'create_by'     => $this->session->id_session,
+            'event_date'    => $this->input->post('event_date'),
+            'chat_date'    => $this->input->post('chat_date'),
+            'location'      => $this->input->post('location'),
+            'create_date'   => $date_create
+        );
+    
+        // Insert ke tabel projects
+        $this->Potensial_model->insert_potensial_clients($data);
+
+        $data_log = array(
+
+            'log_activity_user_id'=>$this->session->id_session,
+            'log_activity_modul' => 'potensial-clients/create',
+            'log_activity_document_no' => $id_session2,
+            'log_activity_status' => 'Tambah Potensial Klien ',
+            'log_activity_platform'=> $agent,
+            'log_activity_ip'=> $this->input->ip_address()
+            
+        );
+
+        $this->Potensial_model->insert_log_activity($data_log);
+    
+    
+        $this->session->set_flashdata('Success', 'Potensial klien berhasil dibuat');
+        redirect('potensial-clients');
+    }
+
+    public function lihat($id_session) {
+        if ($this->session->level=='1' OR $this->session->level=='2' OR $this->session->level=='3' OR $this->session->level=='4' OR $this->session->level=='5'){
+            cek_session_akses('potensial-clients',$this->session->id_session);
+        $data['pc'] = $this->Potensial_model->get_potensial_clients_by_session($id_session);
+        $data['logactivity'] = $this->Potensial_model->get_logactivity_by_session($id_session);
+        $this->load->view('potensial_clients/lihat', $data);
+         }else{
+                redirect(base_url());
+            }
+    }
+
+    public function edit($id_session) {
+        if ($this->session->level=='1' OR $this->session->level=='2' OR $this->session->level=='3' OR $this->session->level=='4' OR $this->session->level=='5'){
+            cek_session_akses('potensial-clients',$this->session->id_session);
+        $data['pc'] = $this->Potensial_model->get_potensial_clients_by_session($id_session);
+        $this->load->view('potensial_clients/edit', $data);
+         }else{
+                redirect(base_url());
+            }
+    }
+
+    public function update($id_session) {
+
+        if ($this->agent->is_browser()) // Agent untuk fitur di log activity
+                {
+                      $agent = 'Desktop ' .$this->agent->browser().' '.$this->agent->version();
+                }
+                elseif ($this->agent->is_robot())
+                {
+                      $agent = $this->agent->robot();
+                }
+                elseif ($this->agent->is_mobile())
+                {
+                      $agent = 'Mobile' .$this->agent->mobile().''.$this->agent->version();
+                }
+                else
+                {
+                      $agent = 'Unidentified User Agent';
+                }
+        $data = array(
+            'pc_name'  => $this->input->post('pc_name'),
+            'pc_nowa'  => $this->input->post('pc_nowa'),
+            'event_date'    => $this->input->post('event_date'),
+            'chat_date'    => $this->input->post('chat_date'),
+            'location'      => $this->input->post('location'),
+            'status'      => $this->input->post('status'),
+            'note'      => $this->input->post('note'),
+        );
+    
+        $this->Potensial_model->update_potensial_clients($id_session, $data);
+        $status = 'Edit ' .$this->input->post('status');
+
+
+        $data_log = array(
+
+            'log_activity_user_id'=>$this->session->id_session,
+            'log_activity_modul' => 'potensial-clients/edit',
+            'log_activity_document_no' => $id_session,
+            'log_activity_status' => $status,
+            'log_activity_platform'=> $agent,
+            'log_activity_ip'=> $this->input->ip_address()
+            
+        );
+
+        $this->Potensial_model->insert_log_activity($data_log);
+    
+        $this->session->set_flashdata('Success', 'Projects berhasil diupdate');
+        redirect('potensial-clients');
+    }
+
+    public function delete($id_session) {
+        if ($this->session->level=='1' OR $this->session->level=='2' OR $this->session->level=='3' OR $this->session->level=='4' OR $this->session->level=='5'){
+            cek_session_akses('potensial-clients',$this->session->id_session);
+        
+
+        if ($this->agent->is_browser()) // Agent untuk fitur di log activity
+                {
+                      $agent = 'Desktop ' .$this->agent->browser().' '.$this->agent->version();
+                }
+                elseif ($this->agent->is_robot())
+                {
+                      $agent = $this->agent->robot();
+                }
+                elseif ($this->agent->is_mobile())
+                {
+                      $agent = 'Mobile' .$this->agent->mobile().''.$this->agent->version();
+                }
+                else
+                {
+                      $agent = 'Unidentified User Agent';
+                }
+
+        $data = ['status' => 'delete'];
+        $this->Potensial_model->update_potensial_clients($id_session, $data);
+        $data_log = array(
+
+            'log_activity_user_id'=>$this->session->id_session,
+            'log_activity_modul' => 'potensial-clients/delete',
+            'log_activity_document_no' => $id_session,
+            'log_activity_status' => 'Hapus',
+            'log_activity_platform'=> $agent,
+            'log_activity_ip'=> $this->input->ip_address()
+            
+        );
+
+        $this->Potensial_model->insert_log_activity($data_log);
+
+
+        $this->session->set_flashdata('Success', 'Potensial klien berhasil dihapus');
+        redirect('potensial-clients');
+         }else{
+                redirect(base_url());
+            }
+    }
+
+    public function recycle_bin() {
+        $data['potensial_clients'] = $this->Potensial_model->get_deleted_potensial_clients();  // Get projects with status 'delete'
+        $this->load->view('potensial_clients/recycle_bin', $data);
+    }    
+
+    public function restore($id_session) {
+
+        if ($this->agent->is_browser()) // Agent untuk fitur di log activity
+                {
+                      $agent = 'Desktop ' .$this->agent->browser().' '.$this->agent->version();
+                }
+                elseif ($this->agent->is_robot())
+                {
+                      $agent = $this->agent->robot();
+                }
+                elseif ($this->agent->is_mobile())
+                {
+                      $agent = 'Mobile' .$this->agent->mobile().''.$this->agent->version();
+                }
+                else
+                {
+                      $agent = 'Unidentified User Agent';
+                }
+        $data = ['status' => 'Tanya-tanya'];
+        $this->Potensial_model->update_potensial_clients($id_session, $data);
+
+        $data_log = array(
+
+            'log_activity_user_id'=>$this->session->id_session,
+            'log_activity_modul' => 'potensial-clients/restore',
+            'log_activity_document_no' => $id_session,
+            'log_activity_status' => 'Restore',
+            'log_activity_platform'=> $agent,
+            'log_activity_ip'=> $this->input->ip_address()
+            
+        );
+
+        $this->Potensial_model->insert_log_activity($data_log);      
+    
+        $this->session->set_flashdata('Success', 'Potensial klien berhasil dipulihkan');
+        redirect('potensial-clients');
+    }
+
+    public function permanent_delete($id_session) {
+        $this->Potensial_model->delete_potensial_clients_permanent($id_session);
+    
+          
+        $this->session->set_flashdata('Success', 'Potensial klien berhasil dihapus permanen');
+        redirect('potensial_clients/recycle_bin');
+    }
+    
+}
