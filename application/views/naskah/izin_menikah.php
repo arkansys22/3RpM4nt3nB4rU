69acda5dd114d27492f8365b6a78c4c2ac5ad60a
galihnkk <galihnkk96@gmail.com>
2025-03-15 14:19:41 +0700

    adjustment

--- application/views/naskah/izin_menikah.php
+++ application/views/naskah/izin_menikah.php
@@ -26,30 +26,33 @@
         <p class="text-lg leading-relaxed">Bismillahirrohmanirrohim,</p>
         <p class="text-lg leading-relaxed">Astagfirullahal’adzim 3x</p>
         <p class="text-lg leading-relaxed">Asyhadualla illa ha illalah, wa asyhadu anna muhammadarrosulullah.</p>
-        <p class="text-lg leading-relaxed"><?= $client->f_bride_fathercname; ?> dan <?= $client->f_bride_mothercname; ?> yang <?= $client->f_bride_cname; ?> cintai dan hormati, <?= $client->f_bride_cname; ?> bersyukur dan berterima
+        <p class="text-lg leading-relaxed"><?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?>
+        dan <?php if (!empty($client->f_bride_mreplacementcname)) {echo $client->f_bride_mreplacementcname;} else {echo $client->f_bride_mothercname;}?> yang <?= $client->f_bride_cname; ?> cintai dan hormati, <?= $client->f_bride_cname; ?> bersyukur dan berterima
         kasih kepada Allah SWT karena telah diberikan limpahan perhatian, kasih
         sayang dan cinta kasih pada <?= $client->f_bride_cname; ?> tiada henti.</p>
         <p class="text-lg leading-relaxed text-justify"><?= $client->f_bride_cname; ?> menghaturkan permohonan maaf yang sedalam-dalamnya atas
         segala kehilafan dan kesalahan <?= $client->f_bride_cname; ?>, baik kata-kata maupun perbuatan
-        yang menyakiti <?= $client->f_bride_fathercname; ?> dan <?= $client->f_bride_mothercname; ?>.</p>
+        yang menyakiti <?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?>
+        dan <?php if (!empty($client->f_bride_mreplacementcname)) {echo $client->f_bride_mreplacementcname;} else {echo $client->f_bride_mothercname;}?>.</p>
         <p class="text-lg leading-relaxed text-justify">Hari ini <?= hari($client->wedding_date) ?>, <?= tgl_indo($client->wedding_date) ?>, <?= $client->f_bride_cname; ?> memohon izin dan memohon
         restu untuk dinikahkan dengan lelaki pilihan <?= $client->f_bride_cname; ?>, untuk menemani
         perjalanan panjang hidup <?= $client->f_bride_cname; ?> kelak.</p>
         <p class="text-lg leading-relaxed text-justify">Seorang laki-laki bernama <?= $client->m_bride_fname; ?>, yang Insha’Allah
         bisa menjadi imam yang bijak dan penuh kasih sayang.</p></br>
 
-        <h1 class="text-xl font-bold text-center mb-4">Permohonan Izin Menikah (<?= $client->f_bride_fathercname; ?>)</h1>
+        <h1 class="text-xl font-bold text-center mb-4">Permohonan Izin Menikah (<?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?>)</h1>
 </br>
         <p class="text-lg leading-relaxed text-justify">Putriku <?= $client->f_bride_cname; ?>, penyampaian izin pernikahanmu dan permohonan restumu
-        sudah <?= $client->f_bride_fathercname; ?> restui dan <?= $client->f_bride_fathercname; ?> dengar dengan seksama.</p>
-        <p class="text-lg leading-relaxed text-justify">Karena Insya Allah sebentar lagi <?= $client->f_bride_fathercname; ?> akan segera menikahkanmu
+        sudah <?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?> restui dan <?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?> dengar dengan seksama.</p>
+        <p class="text-lg leading-relaxed text-justify">Karena Insya Allah sebentar lagi <?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?> akan segera menikahkanmu
         dengan calon suamimu yang bernama <?= $client->m_bride_fname; ?>.</p>
-        <p class="text-lg leading-relaxed text-justify">Teriring doa, semoga Allah meridhoi hajat pernikahan yang akan <?= $client->f_bride_fathercname; ?>
+        <p class="text-lg leading-relaxed text-justify">Teriring doa, semoga Allah meridhoi hajat pernikahan yang akan <?php if (!empty($client->f_bride_freplacementcname)) {echo $client->f_bride_freplacementcname;} else {echo $client->f_bride_fathercname;}?>
         langsungkan sebentar lagi. Hingga rumah tanggamu nanti senantiasa
         rukun, damai dan bahagia penuh rahmat dan keberkahan dari Allah SWT.</p>
         <p class="text-lg leading-relaxed">Aamiin aamiin Allahumma aamiin..</p>
     </br>
 
+    <?php if (empty($client->f_bride_freplacementname)): ?>
     <div class="w-full sm:w-3/5 lg:w-2/5 mx-auto border-2 border-black p-4 text-center">
         <p class="text-lg sm:text-xl leading-relaxed font-semibold text-justify">
             Saya terima nikah dan
@@ -58,6 +61,7 @@
             mas kawin tersebut dibayar tunai
         </p>
     </div>
+    <?php endif; ?>
     <div class="mt-6 flex justify-between no-print">
     <div class="flex">
         <a href="<?= base_url('naskah/izin_menikah/pdf/' . $client->id_session); ?>" 
