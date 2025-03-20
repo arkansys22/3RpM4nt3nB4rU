0814ad877fac1e5cceb2134c38b5036a7d2c9779
arkans <82564511+arkansys22@users.noreply.github.com>
2025-03-20 15:53:45 +0700

    up

--- application/controllers/Crud_user.php
+++ application/controllers/Crud_user.php
@@ -12,11 +12,11 @@ class crud_user extends CI_Controller {
     public function index() {
         if ($this->session->level=='1'){
         cek_session_akses_developer('user',$this->session->id_session);
-        $data['users'] = $this->users_model->get_all_user(); // Ubah pemanggilan model
+        $data['users'] = $this->Users2_model->get_all_user(); // Ubah pemanggilan model
         $this->load->view('user/index', $data);
         }else if ($this->session->level=='4'){
         cek_session_akses_staff_admin('user',$this->session->id_session);
-        $data['users'] = $this->users_model->get_all_user(); // Ubah pemanggilan model
+        $data['users'] = $this->Users2_model->get_all_user(); // Ubah pemanggilan model
         $this->load->view('user/index', $data);
 
         }else{
@@ -77,7 +77,7 @@ class crud_user extends CI_Controller {
         );
     
         // Insert ke tabel projects
-        $this->users_model->insert_users($data);
+        $this->Users2_model->insert_users($data);
 
         $data_log = array(
             'log_activity_user_id'=>$this->session->id_session,
@@ -87,7 +87,7 @@ class crud_user extends CI_Controller {
             'log_activity_platform'=> $agent,
             'log_activity_ip'=> $this->input->ip_address()            
         );
-        $this->users_model->insert_log_activity($data_log);   
+        $this->Users2_model->insert_log_activity($data_log);   
     
         $this->session->set_flashdata('Success', 'Pengguna berhasil dibuat');
         redirect('user');
@@ -96,15 +96,15 @@ class crud_user extends CI_Controller {
     public function lihat($id_session) {
         if ($this->session->level=='1' OR $this->session->level=='2' OR $this->session->level=='3' OR $this->session->level=='5'){
             cek_session_akses_developer('user',$this->session->id_session);
-            $data['pc'] = $this->users_model->get_users_by_session($id_session);
-            $data['logactivity'] = $this->users_model->get_logactivity_by_session($id_session);
+            $data['pc'] = $this->Users2_model->get_users_by_session($id_session);
+            $data['logactivity'] = $this->Users2_model->get_logactivity_by_session($id_session);
             $this->load->view('user/lihat', $data);
 
 
         }else if($this->session->level=='4'){
             cek_session_akses_staff_admin('user',$this->session->id_session);
-            $data['pc'] = $this->users_model->get_users_by_session($id_session);
-            $data['logactivity'] = $this->users_model->get_logactivity_by_session($id_session);
+            $data['pc'] = $this->Users2_model->get_users_by_session($id_session);
+            $data['logactivity'] = $this->Users2_model->get_logactivity_by_session($id_session);
             $this->load->view('user/lihat', $data);
 
         }else{
@@ -118,7 +118,7 @@ class crud_user extends CI_Controller {
             $data['level'] = $this->Crud_m->view_ordering('user_level','user_level_id','asc');
             $data['crews'] = $this->Crud_m->view_ordering('crews','id','asc');
             $data['clients'] = $this->Crud_m->view_ordering('clients','id','asc');
-            $data['pc'] = $this->users_model->get_users_by_session($id_session);
+            $data['pc'] = $this->Users2_model->get_users_by_session($id_session);
             $this->load->view('user/edit', $data);
             
         }else if($this->session->level=='4'){
@@ -126,7 +126,7 @@ class crud_user extends CI_Controller {
             $data['level'] = $this->Crud_m->view_ordering('user_level','user_level_id','asc');
             $data['crews'] = $this->Crud_m->view_ordering('crews','id','asc');
             $data['clients'] = $this->Crud_m->view_ordering('clients','id','asc');
-            $data['pc'] = $this->users_model->get_users_by_session($id_session);
+            $data['pc'] = $this->Users2_model->get_users_by_session($id_session);
             $this->load->view('user/edit', $data);
 
         }else{
@@ -174,7 +174,7 @@ class crud_user extends CI_Controller {
 
         }
     
-        $this->users_model->update_users($id_session, $data);
+        $this->Users2_model->update_users($id_session, $data);
         $status = 'Edit Pengguna';
 
 
@@ -189,7 +189,7 @@ class crud_user extends CI_Controller {
             
         );
 
-        $this->users_model->insert_log_activity($data_log);
+        $this->Users2_model->insert_log_activity($data_log);
     
         $this->session->set_flashdata('Success', 'Pengguna berhasil diupdate');
         redirect('user/lihat/' . $id_session);
@@ -218,7 +218,7 @@ class crud_user extends CI_Controller {
                 }
 
         $data = ['user_stat' => 'Delete'];
-        $this->users_model->update_users($id_session, $data);
+        $this->Users2_model->update_users($id_session, $data);
         $data_log = array(
 
             'log_activity_user_id'=>$this->session->id_session,
@@ -230,7 +230,7 @@ class crud_user extends CI_Controller {
             
         );
 
-        $this->users_model->insert_log_activity($data_log);
+        $this->Users2_model->insert_log_activity($data_log);
 
 
         $this->session->set_flashdata('Success', 'Pengguna berhasil dihapus');
@@ -241,7 +241,7 @@ class crud_user extends CI_Controller {
     }
 
     public function recycle_bin() {
-        $data['users'] = $this->users_model->get_deleted_user();  // Get projects with status 'delete'
+        $data['users'] = $this->Users2_model->get_deleted_user();  // Get projects with status 'delete'
         $this->load->view('user/recycle_bin', $data);
     }    
 
@@ -264,7 +264,7 @@ class crud_user extends CI_Controller {
                       $agent = 'Unidentified User Agent';
                 }
         $data = ['user_stat' => 'Publish'];
-        $this->users_model->update_Users($id_session, $data);
+        $this->Users2_model->update_Users($id_session, $data);
 
         $data_log = array(
 
@@ -277,7 +277,7 @@ class crud_user extends CI_Controller {
             
         );
 
-        $this->users_model->insert_log_activity($data_log);      
+        $this->Users2_model->insert_log_activity($data_log);      
     
         $this->session->set_flashdata('Success', 'Pengguna berhasil dipulihkan');
         redirect('user');
@@ -306,7 +306,7 @@ class crud_user extends CI_Controller {
                 }
 
 
-                $this->users_model->delete_Users_permanent($id_session);
+                $this->Users2_model->delete_Users_permanent($id_session);
 
                 $data_log = array(
 
@@ -319,7 +319,7 @@ class crud_user extends CI_Controller {
                     
                 );
 
-                $this->users_model->insert_log_activity($data_log);
+                $this->Users2_model->insert_log_activity($data_log);
     
         
         $this->session->set_flashdata('Success', 'Pengguna berhasil dihapus permanen');
