47b92d4a8ab091cdd47e0d7ed9cfa43e7924ee9e
galihnkk <galihnkk96@gmail.com>
2025-04-09 10:31:39 +0700

    add level 7 and crew_lihat

--- application/controllers/Crud_project.php
+++ application/controllers/Crud_project.php
@@ -41,6 +41,11 @@ class Crud_project extends CI_Controller {
             $data['aaa'] = '';
             $this->load->view('backend/v_home', $data);
             
+        }else if($this->session->level=='7'){
+            cek_session_akses_special_role('project',$this->session->id_session);
+            $data['project'] = $this->project_model->get_all_project();
+            $this->load->view('project/index', $data);
+
         }else{
             redirect(base_url());
             }
@@ -173,6 +178,32 @@ class Crud_project extends CI_Controller {
         $data['logactivity'] = $this->project_model->get_logactivity_by_session($id_session);
     
         $this->load->view('project/lihat', $data);
+    }
+
+    public function crew_lihat($id_session) {
+        if ($this->session->level == '1') {
+            cek_session_akses_developer('project', $this->session->id_session);
+        } else if ($this->session->level == '2') {
+            cek_session_akses_administrator('project', $this->session->id_session);
+        } else if ($this->session->level == '4') {
+            cek_session_akses_staff_admin('project', $this->session->id_session);
+        } else if ($this->session->level == '7') {
+            cek_session_akses_staff('project', $this->session->id_session);
+        } else {
+            redirect(base_url());
+            return;
+        }
+
+        $data['project'] = $this->project_model->get_project_by_session($id_session);
+        $data['crew_list'] = $this->CrewProjects_model->get_crew_by_project($id_session);
+        $data['vendors'] = $this->Vendor_model->get_vendor_by_id($id_session);
+        $data['logactivity'] = $this->project_model->get_logactivity_by_session($id_session);
+
+        if (!$data['project']) {
+            show_error('Project not found.', 404);
+        }
+
+        $this->load->view('project/crew_lihat', $data);
     }
 
     public function edit($id_session) {
