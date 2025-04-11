55362c5ce4e2913fe71b958380fd218aa217e2d0
arkans <82564511+arkansys22@users.noreply.github.com>
2025-04-11 18:48:38 +0700

    up

--- application/views/project/lihat.php
+++ application/views/project/lihat.php
@@ -246,7 +246,7 @@
 </div>
 
                 <a href="<?= site_url('project/edit/'. $project->id_session) ?>" class="ml-2 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 inline-block text-center w-auto">Edit Project</a>
-                <a href="javascript:history.back()" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 inline-block text-center w-auto">Kembali</a>
+                <a href="<?= site_url('project') ?>" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 inline-block text-center w-auto">Kembali</a>
               </form>
 
               <!-- ====== Table Three Start -->
