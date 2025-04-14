5ce70717cf906e8ed7a3667df7b7a40d5511cf56
galihnkk <galihnkk96@gmail.com>
2025-04-14 11:29:51 +0700

    test

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -13,8 +13,8 @@
 					<span class="relative block h-5.5 w-5.5 cursor-pointer">
 					  <span class="du-block absolute right-0 h-full w-full">
 						<span
-						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white"
-						  :class="{ '!w-full delay-300': !sidebarToggle }"
+						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out"
+						  :class="{ '!w-full delay-300': !sidebarToggle, 'bg-black': !darkMode, 'bg-white': darkMode }"
 						></span>
 						<span
 						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white"
