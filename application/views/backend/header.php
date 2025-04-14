22d2c6b0c0c151af8d07519f1c6ce8ca89040678
galihnkk <galihnkk96@gmail.com>
2025-04-14 11:31:48 +0700

    test

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -17,22 +17,22 @@
 						  :class="{ '!w-full delay-300': !sidebarToggle, 'bg-black': !darkMode, 'bg-white': darkMode }"
 						></span>
 						<span
-						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white"
-						  :class="{ '!w-full delay-400': !sidebarToggle }"
+						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out"
+						  :class="{ '!w-full delay-400': !sidebarToggle 'bg-black': !darkMode, 'bg-white': darkMode }"
 						></span>
 						<span
-						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white"
-						  :class="{ '!w-full delay-500': !sidebarToggle }"
+						  class="relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out"
+						  :class="{ '!w-full delay-500': !sidebarToggle 'bg-black': !darkMode, 'bg-white': darkMode }"
 						></span>
 					  </span>
 					  <span class="du-block absolute right-0 h-full w-full rotate-45">
 						<span
-						  class="absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white"
-						  :class="{ '!h-0 delay-[0]': !sidebarToggle }"
+						  class="absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out"
+						  :class="{ '!h-0 delay-[0]': !sidebarToggle 'bg-black': !darkMode, 'bg-white': darkMode }"
 						></span>
 						<span
-						  class="delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white"
-						  :class="{ '!h-0 delay-200': !sidebarToggle }"
+						  class="delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out"
+						  :class="{ '!h-0 delay-200': !sidebarToggle 'bg-black': !darkMode, 'bg-white': darkMode }"
 						></span>
 					  </span>
 					</span>
