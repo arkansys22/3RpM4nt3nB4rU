338f9a3c43bdd6b4e288e3247609ed7f59ba1c4d
galihnkk <galihnkk96@gmail.com>
2025-04-15 15:37:38 +0700

    change back to potensial-client

--- application/views/potensial_clients/lihat.php
+++ application/views/potensial_clients/lihat.php
@@ -43,7 +43,7 @@
                 <label class="block mb-2"><strong>Catatan : </strong><?= $pc->note ?></label>
                 <br>
                 <a href="<?= site_url('potensial-clients/edit/'. $pc->id_session) ?>" class="ml-2 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 inline-block text-center w-auto">Edit</a>
-                <a href="javascript:history.back()" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 inline-block text-center w-auto">Kembali</a>
+                <a href="<?= site_url('potensial-clients') ?>" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 inline-block text-center w-auto">Kembali</a>
               </form>
 
               <!-- ====== Table Three Start -->
