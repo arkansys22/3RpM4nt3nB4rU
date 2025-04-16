19bae33d174e661bc7f2a371a6ff9fb2e9e41f1e
galihnkk <galihnkk96@gmail.com>
2025-04-16 17:14:26 +0700

    add get revenue per year and with transaction id

--- application/models/Payment_model.php
+++ application/models/Payment_model.php
@@ -48,4 +48,38 @@ class Payment_model extends CI_Model {
         return $this->db->insert('log_activity', $data_log);
     }
 
+    public function get_revenue_per_year() {
+        $this->db->select("YEAR(date) as year, MONTH(date) as month, SUM(total_bill) as total_bill, SUM(CASE WHEN status = 'Paid' THEN total_paid ELSE 0 END) as total_paid");
+        $this->db->from("payment");
+        $this->db->group_by(["YEAR(date)", "MONTH(date)"]);
+        $this->db->order_by("YEAR(date) DESC, MONTH(date) DESC");
+        $query = $this->db->get();
+
+        $result = $query->result_array();
+        $revenue_per_year = [];
+        foreach ($result as $row) {
+            $revenue_per_year[$row['year']][$row['month']] = [
+                'total_bill' => $row['total_bill'],
+                'total_paid' => $row['total_paid']
+            ];
+        }
+        return $revenue_per_year;
+    }
+
+    public function get_paid_revenues_with_transaction_id($month, $year) {
+        $this->db->select('id_session, transactions_id, total_bill, total_paid, date as transaction_date');
+        $this->db->from('payment');
+        $this->db->where('MONTH(date)', $month);
+        $this->db->where('YEAR(date)', $year);
+        $this->db->where('transactions_id IS NOT NULL'); // Ensure transaction_id exists
+        $this->db->group_start(); // Start grouping conditions
+        $this->db->where('total_bill IS NOT NULL'); // Include if total_bill exists
+        $this->db->or_group_start(); // Start sub-group for total_paid condition
+        $this->db->where('total_paid IS NOT NULL'); // Include if total_paid exists
+        $this->db->where('status', 'Paid'); // Only include if status is Paid
+        $this->db->group_end(); // End sub-group
+        $this->db->group_end(); // End grouping conditions
+        return $this->db->get()->result();
+    }
+
 }
