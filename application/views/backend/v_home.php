b7caf286f981a33ba14de7a38a8e1866c1da2fb8
galihnkk <galihnkk96@gmail.com>
2025-04-21 15:35:45 +0700

    change button 1 to Event

--- application/views/backend/v_home.php
+++ application/views/backend/v_home.php
@@ -244,9 +244,14 @@
 				<div class="col-span-12">
 				    <div class="flex flex-wrap justify-center gap-4">
 				        <!-- Tombol 1 -->
-				        <button class="w-40 h-40 bg-fuchsia-700 text-white font-semibold rounded-none hover:bg-fuchsia-500 focus:outline-none">
-				            Button 1
-				        </button>
+						<a href="<?= base_url('panel/staff') ?>">
+							<button class="w-40 h-40 bg-fuchsia-700 text-white font-semibold rounded-none hover:bg-fuchsia-500 focus:outline-none flex flex-col items-center justify-center">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mb-2">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+								</svg>
+								Event
+							</button>
+						</a>
 				        <!-- Tombol 2 -->
 				        <button class="w-40 h-40 bg-fuchsia-700 text-white font-semibold rounded-none hover:bg-fuchsia-500 focus:outline-none">
 				            Button 2
