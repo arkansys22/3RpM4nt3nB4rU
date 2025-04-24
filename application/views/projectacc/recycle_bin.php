61620ae69245bdb4aeae88dbb88c16b469abc196
galihnkk <galihnkk96@gmail.com>
2025-04-24 12:23:30 +0700

    change kembali button place and using an icon now on projectacc recycle bin

--- application/views/projectacc/recycle_bin.php
+++ application/views/projectacc/recycle_bin.php
@@ -32,10 +32,14 @@
             <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
                 <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
                     <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-                        <h1 class="text-2xl font-bold mb-4">Recycle Bin Project</h1>
-
-                    <!-- Tombol Kembali -->
-                    <a href="<?= site_url('project') ?>" class="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-700 focus:outline-none">Kembali</a>
+                        <div class="flex items-center justify-between mb-4">
+                          <h1 class="text-2xl font-bold">Recycle Bin Project</h1>
+                          <a href="<?= site_url('project') ?>" class="flex items-center gap-2 bg-blue-500 text-white p-3 rounded-md hover:bg-blue-700 focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                            </svg>
+                          </a>
+                        </div>
 
                     <!-- ====== Data Table Two Start --><br><br>
                     <div class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
