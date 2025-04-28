16555e2b8f951ae9eb059cd4efb6b91d084ad8c1
galihnkk <galihnkk96@gmail.com>
2025-04-28 15:58:09 +0700

    add logo for jubir cpp and cpw on naskah

--- application/views/naskah/jubir_cpp.php
+++ application/views/naskah/jubir_cpp.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Naskah Jubir CPP</title>
+    <link rel="icon" href="<?php echo base_url()?>assets/backend/mb.png" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @media print {
