df9ccb16acacf0f04128a0908195af682c0963a1
galihnkk <galihnkk96@gmail.com>
2025-04-28 16:20:10 +0700

    add logo for 404

--- application/views/404.php
+++ application/views/404.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Page</title>
+    <title>Data Tidak Ditemukan</title>
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700" rel="stylesheet">
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="icon" href="<?php echo base_url()?>assets/backend/mb.png" type="image/x-icon">
     <style>
         /* CSS code */
         body {
