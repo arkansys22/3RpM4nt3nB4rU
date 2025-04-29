8544d0608516c94421b54354a87dde9be245a3d9
galihnkk <galihnkk96@gmail.com>
2025-04-29 12:25:28 +0700

    add button for restock

--- application/views/supplies/index.php
+++ application/views/supplies/index.php
@@ -36,13 +36,22 @@
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
               <h1 class="text-2xl font-bold mb-4">Daftar Stocks</h1>
               <div class="flex justify-between mb-4">
-              <a href="<?= site_url('supplies/create') ?>">
-                <button class="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-700 focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 5v14m7-7H5"></path>
-                    </svg>
-                </button>
-              </a>
+              <div class="flex space-x-2">
+                <a href="<?= site_url('supplies/create') ?>">
+                  <button class="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-700 focus:outline-none">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 5v14m7-7H5"></path>
+                      </svg>
+                  </button>
+                </a>
+                <a href="<?= site_url('supplies/restock') ?>">
+                  <button class="bg-orange-500 text-white p-3 rounded-md hover:bg-orange-700 focus:outline-none">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                  </svg>
+                  </button>
+                </a>
+              </div>
               <a href="<?= site_url('supplies/recycle_bin') ?>">
                 <button class="bg-red-500 text-white p-3 rounded-md hover:bg-red-700 focus:outline-none">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
