0df5156d418ab6a250c70b5293a87b5d20d08753
galihnkk <galihnkk96@gmail.com>
2025-04-29 17:29:59 +0700

    change what is suppose to

--- application/views/projectacc/create.php
+++ application/views/projectacc/create.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Project</title>
+    <title>Tambah Transaksi</title>
     <link rel="icon" href="<?php echo base_url()?>assets/backend/mb.png" type="image/x-icon">
     <link href="<?php echo base_url()?>assets/backend/style.css" rel="stylesheet" type="text/css"/>
     <script src="https://cdn.tailwindcss.com"></script>
@@ -34,41 +34,34 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h1 class="text-2xl font-bold mb-4">Tambah Project</h1>
-              <form action="<?= site_url('project/store') ?>" method="post" class="bg-white p-6 shadow-md rounded">
-                <label class="block mb-2">Nama Project</label>
-                <input type="text" name="project_name" class="w-full px-4 py-2 border rounded mb-4" required>
+              <h1 class="text-2xl font-bold mb-4">Finance Project <?= $project->project_name ?> <?= $project->event_date ?> | <?= $project->location ?></h1>
+              <form action="<?= site_url('finance-project/store/'.$project->id_session) ?>" method="post" class="bg-white p-6 shadow-md rounded">
 
-                <label class="block mb-2">Nama Client</label>
-                <input type="text" name="client_name" class="w-full px-4 py-2 border rounded mb-4" required>
+                <input type="hidden" name="project_id_session" value="<?= $project->id_session ?>">
 
-                <label class="block mb-2">Tanggal Pernikahan</label>
+                <label class="block mb-2">Nama Transaksi</label>
+                <input type="text" name="nama_transaksi" class="w-full px-4 py-2 border rounded mb-4" required>
+
+                <label class="block mb-2">Tanggal Transaksi</label>
                 <input type="date" name="event_date" class="w-full px-4 py-2 border rounded mb-4" required>
 
-                <label class="block mb-2">Value</label>
+                <label class="block mb-2">Nominal</label>
                 <input type="text" id="formattedNumber" class="w-full px-4 py-2 border rounded mb-4" oninput="formatNumber(this)" name="value" required>
 
+                <label class="block mb-2">Metode Pembayaran</label>
+                <select name="metode" class="w-full px-4 py-2 border rounded mb-4" required>
+                    <option>-- Pilihan Metode Pembayaran --</option>
+                    <option value="Transfer">Transfer Bank</option>
+                    <option value="Cash">Cash</option>
+                </select>
 
                 <label class="block mb-2">Detail</label>
                 <textarea name="detail" class="w-full px-4 py-2 border rounded mb-4" required></textarea>
-
-                <label class="block mb-2">Agama</label>
-                <select name="religion" class="w-full px-4 py-2 border rounded mb-4" required>
-                    <option value="">Pilih Agama</option>
-                    <option value="Islam" <?= set_select('religion', 'Islam') ?>>Islam</option>
-                    <option value="Kristen" <?= set_select('religion', 'Kristen') ?>>Kristen</option>
-                    <option value="Katolik" <?= set_select('religion', 'Katolik') ?>>Katolik</option>
-                    <option value="Hindu" <?= set_select('religion', 'Hindu') ?>>Hindu</option>
-                    <option value="Buddha" <?= set_select('religion', 'Buddha') ?>>Buddha</option>
-                    <option value="Lainnya" <?= set_select('religion', 'Lainnya') ?>>Lainnya</option>
-                  </select>
                 
-                <label class="block mb-2">Lokasi</label>
-                <input type="text" name="location" class="w-full px-4 py-2 border rounded mb-4" required>
 
                 <div class="flex flex-col sm:flex-row justify-end">
-                <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded w-full hover:bg-green-600 sm:w-24 mb-2 sm:mb-0 text-center">Simpan</button>
-                <a href="<?= site_url('project') ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Simpan</button>
+                <a href="javascript:history.back()" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
             </div>
@@ -79,13 +72,15 @@
     </div>
     <!-- ===== Content Area End ===== -->
   </div>
-  <script defer src="<?php echo base_url()?>assets/backend/bundle.js"></script>
+  <script defer src="<?php echo base_url()?>assets/backend/bundle.js">
+  </script>
   <script>
     function formatNumber(input) {
-      let value = input.value.replace(/\D/g, ''); // Hanya angka
-      value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Tambah titik setiap 3 digit
-      input.value = value;
-  }
+        let value = input.value.replace(/\D/g, ''); // Hanya angka
+        value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Tambah titik setiap 3 digit
+        input.value = value;
+    }
   </script>
+
 </body>
 </html>
