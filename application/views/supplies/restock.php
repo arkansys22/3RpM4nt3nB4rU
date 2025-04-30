f4ff4bbef4c68898fce0546db5f7bca82af0618e
galihnkk <galihnkk96@gmail.com>
2025-04-30 16:24:44 +0700

    add button back with icon and change js to url

--- application/views/supplies/restock.php
+++ application/views/supplies/restock.php
@@ -34,7 +34,14 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h1 class="text-2xl font-bold mb-4">Restock Supplies</h1>
+            <div class="flex justify-between items-center mb-4">  
+            <h1 class="text-2xl font-bold mb-4">Restock Supplies</h1>
+              <a href="<?= site_url('supplies') ?>" class="flex items-center gap-2 bg-blue-500 text-white p-3 rounded-md hover:bg-blue-700 focus:outline-none">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                  </svg>
+                </a>
+            </div>
               <div class="overflow-x-auto bg-white dark:bg-neutral-700 rounded-lg shadow-md">
                 <table class="min-w-full text-left text-sm whitespace-nowrap bg-white dark:bg-neutral-800">
                   <thead class="uppercase tracking-wider border-b-2 border-gray-200 dark:border-neutral-600 bg-gray-100 dark:bg-neutral-700">
