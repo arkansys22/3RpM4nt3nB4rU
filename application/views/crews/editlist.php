1519d9514ae2570e07381b7f350ef10003ed2ca8
galihnkk <galihnkk96@gmail.com>
2025-04-30 15:46:37 +0700

    add client name

--- application/views/crews/editlist.php
+++ application/views/crews/editlist.php
@@ -34,7 +34,7 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h1 class="text-2xl font-bold mb-4">Edit Crew for Project</h1>
+              <h1 class="text-2xl font-bold mb-4">Edit Crew dari <?= htmlspecialchars($client_name) ?></h1>
               <form action="<?= site_url('crewproject/updatelist') ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <input type="hidden" name="project_id" value="<?= htmlspecialchars($project_id) ?>">
                 <input type="hidden" name="id_session" value="<?= htmlspecialchars($selected_crew->id_session) ?>">
