f66265e34e4c6ed800339843ccd2455a6671f8d8
galihnkk <galihnkk96@gmail.com>
2025-04-30 15:16:26 +0700

    change jd to url

--- application/views/crews/edit.php
+++ application/views/crews/edit.php
@@ -34,7 +34,7 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h2 class="text-2xl font-bold mb-4">Edit Crew</h2>
+              <h2 class="text-2xl font-bold mb-4">Edit Crew <?= $crews->crew_name ?></h2>
               <form action="<?= site_url('crews/update/'.$crews->id_session) ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <label class="block mb-2">Nama</label>
                 <input type="text" name="crew_name" value="<?= $crews->crew_name ?>" class="w-full px-4 py-2 border rounded mb-4" required>
@@ -69,7 +69,7 @@
 
                 <div class="flex flex-col sm:flex-row justify-end">
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Update</button>
-                <a href="javascript:history.back()" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
+                <a href="<?= site_url('crews/lihat/'.$crews->id_session) ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
             </div>
