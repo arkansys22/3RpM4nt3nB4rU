a600f06707d3a64b2c229c6973e69fd44f098a26
galihnkk <galihnkk96@gmail.com>
2025-05-06 14:40:30 +0700

    make button responsive on mobile

--- application/views/supplies/edit.php
+++ application/views/supplies/edit.php
@@ -43,7 +43,7 @@
                 <input type="text" name="type" value="<?= $supplies->type ?>" class="w-full px-4 py-2 border rounded mb-4" required>
 
                 <div class="flex flex-col sm:flex-row justify-end">
-                  <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Simpan</button>
+                  <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Update</button>
                   <a href="<?= site_url('supplies/lihat/' . $supplies->id_session) ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
