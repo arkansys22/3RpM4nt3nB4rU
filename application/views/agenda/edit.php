4ffcffa136a64a61c5371cf6bb1c790be973d328
galihnkk <galihnkk96@gmail.com>
2025-05-06 13:56:02 +0700

    change some word

--- application/views/agenda/edit.php
+++ application/views/agenda/edit.php
@@ -34,7 +34,7 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h1 class="text-2xl font-bold mb-4">Edit Agenda for <?= $agenda->client_name ?></h1>
+              <h1 class="text-2xl font-bold mb-4">Edit Agenda untuk <?= $agenda->client_name ?></h1>
               <form action="<?= site_url('agenda/update/'.$agenda->id_session) ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <input type="hidden" name="id_session" value="<?= $agenda->id_session ?>">
 
@@ -57,8 +57,8 @@
                 <input type="date" id="honeymoon" name="honeymoon" value="<?= $agenda->honeymoon ?>" class="w-full px-4 py-2 border rounded mb-4">
 
                 <div class="flex flex-col sm:flex-row justify-end">
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Save</button>
-                <a href="<?= site_url('agenda') ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Cancel</a>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Update</button>
+                <a href="<?= site_url('agenda') ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
             </div>
