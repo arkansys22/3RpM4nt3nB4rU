b45ef38cccf47b1e7ae9464e41e2f02bbd9f80ce
galihnkk <galihnkk96@gmail.com>
2025-05-06 14:41:50 +0700

    change some word

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -113,7 +113,7 @@
 
                 <div class="flex flex-col sm:flex-row justify-end">
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Update</button>
-                <a href="<?= site_url('user/lihat/'.$pc->id_session) ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Cancel</a>
+                <a href="<?= site_url('user/lihat/'.$pc->id_session) ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
             </div>
