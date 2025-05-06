3e65b67d420e07d54338d5fc6f5f23d138e10b17
galihnkk <galihnkk96@gmail.com>
2025-05-06 14:37:21 +0700

    make button responsive on mobile

--- application/views/projectacc/create.php
+++ application/views/projectacc/create.php
@@ -60,7 +60,7 @@
                 
 
                 <div class="flex flex-col sm:flex-row justify-end">
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Simpan</button>
+                <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded w-full hover:bg-green-600 sm:w-24 mb-2 sm:mb-0 text-center">Simpan</button>
                 <a href="<?= site_url('finance-project/lihat/'.$project->id_session) ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
