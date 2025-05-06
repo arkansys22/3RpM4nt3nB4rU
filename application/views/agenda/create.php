04b8545993fcda507f5d5740f3534d792382600d
galihnkk <galihnkk96@gmail.com>
2025-05-06 13:55:53 +0700

    change some word

--- application/views/agenda/create.php
+++ application/views/agenda/create.php
@@ -34,7 +34,7 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h1 class="text-2xl font-bold mb-4">Tambah Agenda for <?= $project->client_name ?></h1>
+              <h1 class="text-2xl font-bold mb-4">Tambah Agenda untuk <?= $project->client_name ?></h1>
               <form action="<?= site_url('agenda/store') ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <input type="hidden" name="id_session" value="<?= $project->id_session ?>">
 
