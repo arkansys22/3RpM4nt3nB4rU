50c7313e9e6fc0f1ea0ddee869fd67c6dce3c225
galihnkk <galihnkk96@gmail.com>
2025-05-06 14:25:12 +0700

    make button responsive on mobile

--- application/views/payment/editkwt.php
+++ application/views/payment/editkwt.php
@@ -51,9 +51,9 @@
                 <option value="Paid" <?= $payment->status === 'Paid' ? 'selected' : '' ?>>Paid</option>
             </select>
 
-            <div class="flex justify-end">
-            <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Simpan</button>
-            <a href="<?= base_url('project/lihat/' . $project->id_session) ?>" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">Batal</a>
+            <div class="flex flex-col sm:flex-row justify-end">
+              <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Update</button>
+              <a href="<?= base_url('project/lihat/' . $project->id_session) ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
             </div>
           </form>
         </div>
