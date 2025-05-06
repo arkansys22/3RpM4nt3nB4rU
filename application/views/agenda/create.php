d62266bc5305405db01511b84b6e58906ebdc042
galihnkk <galihnkk96@gmail.com>
2025-05-06 13:52:20 +0700

    change word

--- application/views/agenda/create.php
+++ application/views/agenda/create.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Agenda</title>
+    <title>Tambah Agenda</title>
     <link rel="icon" href="<?php echo base_url()?>assets/backend/mb.png" type="image/x-icon">
     <link href="<?php echo base_url()?>assets/backend/style.css" rel="stylesheet" type="text/css"/>
     <script src="https://cdn.tailwindcss.com"></script>
@@ -34,7 +34,7 @@
         <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-              <h1 class="text-2xl font-bold mb-4">Create Agenda for <?= $project->client_name ?></h1>
+              <h1 class="text-2xl font-bold mb-4">Tambah Agenda for <?= $project->client_name ?></h1>
               <form action="<?= site_url('agenda/store') ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <input type="hidden" name="id_session" value="<?= $project->id_session ?>">
 
@@ -57,8 +57,8 @@
                 <input type="date" id="honeymoon" name="honeymoon" class="w-full px-4 py-2 border rounded mb-4">
 
                 <div class="flex flex-col sm:flex-row justify-end">
-                  <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded w-full hover:bg-green-600 sm:w-24 mb-2 sm:mb-0 text-center">Save</button>
-                  <a href="<?= site_url('agenda') ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Cancel</a>
+                  <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded w-full hover:bg-green-600 sm:w-24 mb-2 sm:mb-0 text-center">Simpan</button>
+                  <a href="<?= site_url('agenda') ?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
             </div>
