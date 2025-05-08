2835f38c173400f853f4ca08b486d6811d6a8f47
galihnkk <galihnkk96@gmail.com>
2025-05-08 14:39:30 +0700

    add partner for create vendor and edit, now can store and edit from partner. max 3MB for photo

--- application/controllers/Crud_vendor.php
+++ application/controllers/Crud_vendor.php
@@ -14,7 +14,8 @@ class Crud_vendor extends CI_Controller {
     public function create($id_session) {
         $data['project'] = $this->project_model->get_project_by_session($id_session);
         $data['vendors'] = $this->Vendor_model->get_vendor_by_id($id_session);
-    
+        $data['partners'] = $this->db->get('partner')->result(); // Fetch list of partners
+
         $this->load->view('vendor/create', $data);
     }    
 
@@ -37,8 +38,9 @@ class Crud_vendor extends CI_Controller {
             $agent = 'Unidentified User Agent';
         }
 
-        $id_session = $this->input->post('id_session'); // Ambil ID session dari form
+        $id_session = $this->input->post('id_session');
         $vendor_id = hash('sha256', bin2hex(random_bytes(16)));
+
         $data = [
             'id_session'    => $id_session,
             'vendor_id'     => $vendor_id,
@@ -48,74 +50,71 @@ class Crud_vendor extends CI_Controller {
             'contact_name'  => $this->input->post('contact_name'),
             'phone'         => $this->input->post('phone'),
             'detail'        => $this->input->post('detail'),
+            'photo1'        => $this->upload_photo('photo1'),
+            'photo2'        => $this->upload_photo('photo2'),
+            'photo3'        => $this->upload_photo('photo3'),
+            'photo4'        => $this->upload_photo('photo4'),
+            'photo5'        => $this->upload_photo('photo5'),
         ];
-    
-        // Konfigurasi upload
-        $config['upload_path']   = './uploads/';
-        $config['allowed_types'] = 'jpg|jpeg|png';
-        $config['max_size']      = 10024; // 2MB
-        $config['encrypt_name']  = TRUE;
-    
-        if (!is_dir($config['upload_path'])) {
-            mkdir($config['upload_path'], 0777, true);
-        }
-    
-        // Upload foto 1-5
-        for ($i = 1; $i <= 5; $i++) {
-            $input_name = 'photo' . $i;
-            if (!empty($_FILES[$input_name]['name'])) {
-                $_FILES['file']['name']     = $_FILES[$input_name]['name'];
-                $_FILES['file']['type']     = $_FILES[$input_name]['type'];
-                $_FILES['file']['tmp_name'] = $_FILES[$input_name]['tmp_name'];
-                $_FILES['file']['error']    = $_FILES[$input_name]['error'];
-                $_FILES['file']['size']     = $_FILES[$input_name]['size'];
-    
-                $this->upload->initialize($config);
-    
-                if ($this->upload->do_upload('file')) {
-                    $upload_data = $this->upload->data();
-                    $data['photo' . $i] = $upload_data['file_name'];
-                } else {
-                    $this->session->set_flashdata('message', '<p style="color:red;">Gagal upload ' . $input_name . ': ' . $this->upload->display_errors() . '</p>');
-                    redirect('vendor/create');
-                }
+
+        if ($this->input->post('vendor_status') === 'partner') {
+            $partner = $this->db->get_where('partner', ['id' => $this->input->post('partner_id')])->row();
+            if ($partner) {
+                $data['vendor_id'] = $partner->id_session;
+                $data['vendor'] = $partner->partner_name;
+                $data['type'] = $partner->type;
+                $data['social_media'] = $partner->social_media;
+                $data['contact_name'] = $partner->contact_name;
+                $data['phone'] = $partner->phone;
+                $data['photo1'] = $partner->logo;
+                $data['detail'] = $this->input->post('partner_detail');
+                $data['photo2'] = $this->upload_photo('partner_photo2');
+                $data['photo3'] = $this->upload_photo('partner_photo3');
+                $data['photo4'] = $this->upload_photo('partner_photo4');
+                $data['photo5'] = $this->upload_photo('partner_photo5');
             }
         }
-    
-        // Simpan ke database
+
         $this->Vendor_model->insert_vendor($data);
 
-        $type =  $this->input->post('type');
-        $status = 'Tambah Vendor '.$type;
+        $type = $this->input->post('type');
+
+        if ($this->input->post('vendor_status') === 'partner') {
+            $partner = $this->db->get_where('partner', ['id' => $this->input->post('partner_id')])->row();
+            if ($partner) {
+            $type = $partner->type; // Ambil type dari tabel partner
+            }
+        }
+        $status = 'Tambah Vendor ' . $type;
         $ip = $this->input->ip_address();
         $location = get_location_from_ip($ip);
         $ip_with_location = $ip . "<br>(" . $location . ")";
 
-        $data_log = array(
-
-            'log_activity_user_id'=>$this->session->id_session,
+        $data_log = [
+            'log_activity_user_id' => $this->session->id_session,
             'log_activity_modul' => 'vendor/create',
             'log_activity_document_no' => $id_session,
             'log_activity_status' => $status,
             'log_activity_waktu' => date('Y-m-d H:i:s'),
-            'log_activity_platform'=> $agent,
-            'log_activity_ip'=> $ip_with_location
-            
-        );
+            'log_activity_platform' => $agent,
+            'log_activity_ip' => $ip_with_location,
+        ];
 
         $this->project_model->insert_log_activity($data_log);
 
         $this->session->set_flashdata('message', '<p style="color:green;">Vendor berhasil disimpan!</p>');
-    
-        // Redirect ke halaman project/lihat/{id_session}
+
         redirect('project/lihat/' . $id_session);
     }
-    
 
     public function edit($id_session, $vendor_id) {
         $data['vendors'] = $this->Vendor_model->get_vendor_by_id_and_vendor_id($id_session, $vendor_id);
         $data['project'] = $this->project_model->get_project_by_session($id_session);
-    
+        $data['partners'] = $this->db->get('partner')->result(); // Fetch list of partners
+
+        // Check if vendor_id exists in the partner table
+        $data['is_partner'] = $this->db->where('id_session', $vendor_id)->count_all_results('partner') > 0;
+
         $this->load->view('vendor/edit', $data);
     }
     
@@ -142,19 +141,39 @@ class Crud_vendor extends CI_Controller {
 
         $old_type = $existing_vendor->type; // Ambil type sebelumnya
 
-        $data = array(
-            'vendor' => $this->input->post('vendor'),
-            'type' => $this->input->post('type'),
-            'social_media' => $this->input->post('social_media'),
-            'contact_name' => $this->input->post('contact_name'),
-            'phone' => $this->input->post('phone'),
-            'detail' => $this->input->post('detail'),
-            'photo1' => $this->upload_photo('photo1') ?: $existing_vendor->photo1,
-            'photo2' => $this->upload_photo('photo2') ?: $existing_vendor->photo2,
-            'photo3' => $this->upload_photo('photo3') ?: $existing_vendor->photo3,
-            'photo4' => $this->upload_photo('photo4') ?: $existing_vendor->photo4,
-            'photo5' => $this->upload_photo('photo5') ?: $existing_vendor->photo5
-        );
+        $data = [];
+
+        $partner_match = $this->db->get_where('partner', ['id_session' => $vendor_id])->row();
+        if ($partner_match) {
+            // Data dari partner
+            $data['vendor_id'] = $partner_match->id_session;
+            $data['vendor'] = $partner_match->partner_name;
+            $data['type'] = $partner_match->type;
+            $data['social_media'] = $partner_match->social_media;
+            $data['contact_name'] = $partner_match->contact_name;
+            $data['phone'] = $partner_match->phone;
+            $data['photo1'] = $partner_match->logo ?: $existing_vendor->photo1;
+        
+            // Editable fields dari form
+            $data['detail'] = $this->input->post('partner_detail') ?: $existing_vendor->detail;
+            $data['photo2'] = $this->upload_photo('partner_photo2') ?: $existing_vendor->photo2;
+            $data['photo3'] = $this->upload_photo('partner_photo3') ?: $existing_vendor->photo3;
+            $data['photo4'] = $this->upload_photo('partner_photo4') ?: $existing_vendor->photo4;
+            $data['photo5'] = $this->upload_photo('partner_photo5') ?: $existing_vendor->photo5;
+        } else {
+            // Non-partner logic tetap sama
+            $data['vendor'] = $this->input->post('vendor');
+            $data['type'] = $this->input->post('type');
+            $data['social_media'] = $this->input->post('social_media');
+            $data['contact_name'] = $this->input->post('contact_name');
+            $data['phone'] = $this->input->post('phone');
+            $data['detail'] = $this->input->post('detail') ?: $existing_vendor->detail;
+            $data['photo1'] = $this->upload_photo('photo1') ?: $existing_vendor->photo1;
+            $data['photo2'] = $this->upload_photo('photo2') ?: $existing_vendor->photo2;
+            $data['photo3'] = $this->upload_photo('photo3') ?: $existing_vendor->photo3;
+            $data['photo4'] = $this->upload_photo('photo4') ?: $existing_vendor->photo4;
+            $data['photo5'] = $this->upload_photo('photo5') ?: $existing_vendor->photo5;
+        }
 
         $this->Vendor_model->update_vendor($id_session, $vendor_id, $data);
 
@@ -164,17 +183,15 @@ class Crud_vendor extends CI_Controller {
         $location = get_location_from_ip($ip);
         $ip_with_location = $ip . "<br>(" . $location . ")";
 
-        $data_log = array(
-
-            'log_activity_user_id'=>$this->session->id_session,
+        $data_log = [
+            'log_activity_user_id' => $this->session->id_session,
             'log_activity_modul' => 'vendor/edit',
             'log_activity_document_no' => $id_session,
             'log_activity_status' => $status,
             'log_activity_waktu' => date('Y-m-d H:i:s'),
-            'log_activity_platform'=> $agent,
-            'log_activity_ip'=> $ip_with_location
-            
-        );
+            'log_activity_platform' => $agent,
+            'log_activity_ip' => $ip_with_location
+        ];
 
         $this->project_model->insert_log_activity($data_log);
 
@@ -275,7 +292,7 @@ class Crud_vendor extends CI_Controller {
     private function upload_photo($input_name) {
         $config['upload_path']   = './uploads/';
         $config['allowed_types'] = 'jpg|jpeg|png';
-        $config['max_size']      = 10024; // 2MB
+        $config['max_size']      = 3072; // 3MB
         $config['encrypt_name']  = TRUE;
 
         if (!is_dir($config['upload_path'])) {
