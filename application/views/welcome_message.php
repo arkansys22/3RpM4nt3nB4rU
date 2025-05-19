a719dbbe741aeef72b65cdc757a48f722d886ade
galihnkk <galihnkk96@gmail.com>
2025-05-19 15:02:20 +0700

    improve the code.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -25,9 +25,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</style>
 </head>
 <body class="bg-white dark:bg-boxdark-2 text-black dark:text-white">
-	<div class="flex h-screen items-center justify-center">
-		<footer class="text-center mt-1 text-sm text-black"><br>
-			<div class="flex justify-center space-x-2 mt-60 md:mt-[31rem]">
+	<div class="min-h-screen flex flex-col justify-end">
+		<footer class="text-center text-sm text-black mb-4">
+			<div class="flex justify-center space-x-2 mb-4">
 				<a href="<?= base_url('login') ?>" class="bg-[#ed126b] hover:bg-pink-600 text-white font-bold py-3 px-6 rounded text-xs flex items-center space-x-2">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-5 h-5">
 						<path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
@@ -41,7 +41,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<span>Chat Admin</span>
 				</a>
 			</div>
-			<p class="mt-5">Copyright © 2025 Mantenbaru Organizer</p>
+			<p>Copyright © 2025 Mantenbaru Organizer</p>
 		</footer>
 	</div>
 	</body>
