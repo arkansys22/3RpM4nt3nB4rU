bc87e0762b3cd4b3acc583854e74afa6226872bd
galihnkk <galihnkk96@gmail.com>
2025-05-20 13:40:27 +0700

    improve the code.

--- application/views/naskah/terima_kasih.php
+++ application/views/naskah/terima_kasih.php
@@ -40,17 +40,14 @@
         <p class="text-lg">Terima kasih.</p>
 
         <div class="mt-6 flex justify-between no-print">
-    <div class="flex">
-        <a href="<?= base_url('naskah/terima_kasih/pdf/' . $client->id_session); ?>" 
-           class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-           Download PDF
-        </a>
-        <button onclick="window.print()" 
-                class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded ml-2">
-            Print
-        </button>
-    </div>
-    <a href="<?= base_url('clients/lihat/' . $client->id_session); ?>" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 inline-block text-center w-auto">Kembali</a>
+            <a href="<?= base_url('naskah/terima_kasih/pdf/' . $client->id_session); ?>" 
+               class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+               Download PDF
+            </a>
+            <button onclick="window.print()" 
+                    class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                Print
+            </button>
         </div>
     </div>
 </body>
