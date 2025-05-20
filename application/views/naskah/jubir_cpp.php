1b31bd55bd63e9d22043b977146dab4825e1fa5a
galihnkk <galihnkk96@gmail.com>
2025-05-20 13:39:26 +0700

    improve the code.

--- application/views/naskah/jubir_cpp.php
+++ application/views/naskah/jubir_cpp.php
@@ -70,18 +70,15 @@
         <p class="text-lg">Billahi taufik wal hidayah wassalamu-alaikum Wr.Wb.</p>
 
         <div class="mt-6 flex justify-between no-print">
-    <div class="flex">
-        <a href="<?= base_url('naskah/jubir_cpp/pdf/' . $client->id_session); ?>" 
-           class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-           Download PDF
-        </a>
-        <button onclick="window.print()" 
-                class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded ml-2">
+            <a href="<?= base_url('naskah/jubir_cpp/pdf/' . $client->id_session); ?>" 
+               class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+               Download PDF
+            </a>
+            <button onclick="window.print()" 
+                class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
             Print
-        </button>
-    </div>
-    <a href="<?= base_url('clients/lihat/' . $client->id_session); ?>" class="ml-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 inline-block text-center w-auto">Kembali</a>
+            </button>
         </div>
-    </div>
+        </div>
 </body>
 </html>
