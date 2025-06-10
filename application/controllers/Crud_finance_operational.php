1b244300b30c5b0d49518fde6bbf80b16759ecae
arkans <82564511+arkansys22@users.noreply.github.com>
2025-06-10 18:12:02 +0700

    up

--- application/controllers/Crud_finance_operational.php
+++ application/controllers/Crud_finance_operational.php
@@ -161,7 +161,7 @@ class crud_finance_operational extends CI_Controller {
             'nama_transaksi'  => $this->input->post('nama_transaksi'),
             'tanggal_transaksi'  => $this->input->post('tanggal_transaksi'),
             'nominal_transaksi'        => str_replace('.', '', $this->input->post('nominal_transaksi')), 
-            'kategori'    => $this->input->post('kategori') 
+            'kategori'    => $this->input->post('kategori'), 
             'periode'    => $this->input->post('periode')                        
             );
          
