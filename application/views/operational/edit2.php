210d3bec9998f062452459491c1c1e8bd55e0c8a
arkans <82564511+arkansys22@users.noreply.github.com>
2025-06-18 16:32:31 +0700

    up

--- application/views/operational/edit2.php
+++ application/views/operational/edit2.php
@@ -70,9 +70,15 @@
                       }} ?>                    
                 </select>
 
+
+                <?php 
+                  $url = isset($_SERVER['HTTP_REFERER']) ? htmlspecialchars($_SERVER['HTTP_REFERER']) : ''; 
+                ?>
+
+
                 <div class="flex flex-col sm:flex-row justify-end">
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600 sm:w-24 mb-2 sm:mb-0 text-center">Update</button>
-                <a href="window.history.back()" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
+                <a href="<?=$url?>" class="sm:ml-2 bg-gray-500 text-white px-4 py-2 rounded w-full hover:bg-gray-600 sm:w-24 text-center">Batal</a>
                 </div>
               </form>
             </div>
