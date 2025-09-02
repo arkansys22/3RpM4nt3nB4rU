78c5e61c43ea578ff9704b3f670fdd362db46ebb
arkans <82564511+arkansys22@users.noreply.github.com>
2025-09-02 17:12:50 +0700

    up

--- application/views/clients/c_edit.php
+++ application/views/clients/c_edit.php
@@ -89,7 +89,7 @@ $islam = strtolower($religion) === 'islam'; // Cek apakah agama Islam
                       <?= !empty($clients->f_bride_freplacementname) ? 'checked' : '' ?>> Meninggal
                   </label>
                   <label>
-                      <input type="radio" name="fayah_status" value="Tidak Ada" id="tidakAdaFayah" onclick="toggleReplacementFields('fayah', true)"
+                      <input type="radio" name="fayah_status" value="Tidak Ada" id="tidakAdaFayah" onclick="toggleReplacementFields('fayah2', true)"
                       <?= !empty($clients->f_bride_freplacementname) ? 'checked' : '' ?>> Tidak Ada
                   </label>
               </div>
@@ -103,6 +103,24 @@ $islam = strtolower($religion) === 'islam'; // Cek apakah agama Islam
 
                 <!-- Nama Lengkap Pengganti Ayah & Nama Panggilan Pengganti Ayah -->
                 <div id="fayah" class="flex-1 <?= !empty($clients->f_bride_freplacementname) ? '' : 'hidden' ?>">
+
+                    <label class="block mb-2" style="color: #000;">Nama Almarhum Ayah Kandung</label>
+                    <input type="text" name="f_bride_fathername" value="<?= $clients->f_bride_fathername ?>" placeholder="Nama Lengkap Ayah" class="w-full px-4 py-2 border rounded mb-4" style="color: #000;">
+
+                    <label class="block mb-2" style="color: #000;">Nama Lengkap Pengganti Ayah</label>
+                    <input type="text" name="f_bride_freplacementname" value="<?= $clients->f_bride_freplacementname ?>" placeholder="Nama Lengkap Pengganti Ayah" class="w-full px-4 py-2 border rounded mb-2" style="color: #000;">
+
+                    <label class="block mb-2" style="color: #000;">Nama Panggilan Pengganti Ayah</label>
+                    <input type="text" name="f_bride_freplacementcname" value="<?= $clients->f_bride_freplacementcname ?>" placeholder="Bapak/Papa/Ayah/Abi/Kaka" class="w-full px-4 py-2 border rounded" style="color: #000;">
+                </div>
+
+
+                <!-- Nama Lengkap Pengganti Ayah & Nama Panggilan Pengganti Ayah -->
+                <div id="fayah2" class="flex-1 <?= !empty($clients->f_bride_freplacementname) ? '' : 'hidden' ?>">
+
+                    <label class="block mb-2" style="color: #000;">Nama Lengkap Ayah Kandung</label>
+                    <input type="text" name="f_bride_fathername" value="<?= $clients->f_bride_fathername ?>" placeholder="Nama Lengkap Ayah" class="w-full px-4 py-2 border rounded mb-4" style="color: #000;">
+
                     <label class="block mb-2" style="color: #000;">Nama Lengkap Pengganti Ayah</label>
                     <input type="text" name="f_bride_freplacementname" value="<?= $clients->f_bride_freplacementname ?>" placeholder="Nama Lengkap Pengganti Ayah" class="w-full px-4 py-2 border rounded mb-2" style="color: #000;">
 
@@ -476,6 +494,7 @@ function toggleReplacementFields(type, show) {
         document.getElementById('fayah-nama-ayah').classList.toggle('hidden', show);
         document.getElementById('fayah-original').classList.toggle('hidden', show);
         document.getElementById('fayah').classList.toggle('hidden', !show);
+        document.getElementById('fayah2').classList.toggle('hidden', !show);
     } else {
         document.getElementById(type).classList.toggle("hidden", !show);
         document.getElementById(type + '-original').classList.toggle("hidden", show);
