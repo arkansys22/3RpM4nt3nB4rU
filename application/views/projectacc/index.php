3cfce1f58d0e8489374793288a11ddf68db4a74a
arkans <82564511+arkansys22@users.noreply.github.com>
2025-10-13 16:43:14 +0700

    up

--- application/views/projectacc/index.php
+++ application/views/projectacc/index.php
@@ -291,7 +291,7 @@
                     $data['modal_ops'] = $this->finance_project_model->get_finance_out($p->id_session); ?>
                     <?php $profit = $data['terbayar_ops']->total_dibayarkan - $data['modal_ops']->total_finance_out?>
 
-                    <?php $presentase = ($profit / $data['terbayar_ops']->total_dibayarkan) * '100' ?>
+                    <?php $presentase = ($profit / $data['terbayar_ops']->total_dibayarkan) * 100 ?>
 
                     <td><?= "Rp " . number_format($profit, 0, ',', '.'); ?> (<?php $persentase ?> %)</td>
                     <td><?= $p->religion ?></td>                    
