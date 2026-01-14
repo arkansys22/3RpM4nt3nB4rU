713eb551ad6dd06a9ba0b8b9ff1de682fd75c8ad
arkans <82564511+arkansys22@users.noreply.github.com>
2026-01-14 18:58:41 +0700

    up

--- application/controllers/Aspanel.php
+++ application/controllers/Aspanel.php
@@ -765,7 +765,7 @@ class Aspanel extends CI_Controller {
 	        ->row();
 
 	    $estimasi_revenue_bulan_ini = $this->db
-	    	->select_sum('total_paid')	    	
+	    	->select_sum('project.value')	    	
 			->join('project', 'project.id_session = payment.id_session')
 			->join('user', 'user.id_session = project.closing_user_idsession')
 	        ->where('DATE(date) >=', $date_start_of_month)
