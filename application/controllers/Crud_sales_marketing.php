e8e35b19d19fcdf709fd0a7c0bc7ef73d244ff22
arkans <82564511+arkansys22@users.noreply.github.com>
2026-01-19 14:45:29 +0700

    up

--- application/controllers/Crud_sales_marketing.php
+++ application/controllers/Crud_sales_marketing.php
@@ -5,7 +5,7 @@ class crud_sales_marketing extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('Operational_model');
+        $this->load->model('Salesmarketing_model');
         $this->load->model('Users2_model');
         $this->load->helper('url');
     }
