cf898dd26ae5873ed9bd7dfcd64a47747ce76cb2
arkans <82564511+arkansys22@users.noreply.github.com>
2026-01-19 19:36:28 +0700

    up

--- application/controllers/Crud_sales_marketing.php
+++ application/controllers/Crud_sales_marketing.php
@@ -84,7 +84,7 @@ class crud_sales_marketing extends CI_Controller {
             for ($m = 1; $m <= 12; $m++) {
                 $periode[] = [
                     'bulan' => str_pad($m, 2, '0', STR_PAD_LEFT),
-                    'label'      => strftime('%B %Y', mktime(0, 0, 0, $m, 1, $year)) // Januari 2026
+                    'label'      => strftime('%B %Y', mktime(0, 0, 0, $m, 1, $year)), // Januari 2026
                     'periode'    => $year . '-' . str_pad($m, 2, '0', STR_PAD_LEFT), // YYYY-MM
                     'tahun' => $year
                 ];
