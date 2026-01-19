582c8d71f181dd950cc2115b7a47f6ad7f51f8bc
arkans <82564511+arkansys22@users.noreply.github.com>
2026-01-19 16:54:42 +0700

    up

--- application/controllers/Aspanel.php
+++ application/controllers/Aspanel.php
@@ -753,6 +753,17 @@ class Aspanel extends CI_Controller {
 
 
 
+	    $bulan = date('m');
+		$tahun = date('Y');
+
+		$target_bulan_ini = $this->db
+		    ->select('targetsales.targetsales_nominal')
+		    ->join('user', 'user.id_session = targetsales.user_id_session')
+		    ->where('MONTH(targetsales.targetsales_periode)', $bulan)
+		    ->where('YEAR(targetsales.targetsales_periode)', $tahun)
+		    ->where('user.id_session', $this->session->id_session)
+		    ->get('targetsales')
+		    ->row();
 
 
 	    // Revenue bulan ini  
