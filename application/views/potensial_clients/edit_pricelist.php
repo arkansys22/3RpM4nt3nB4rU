dbdf2de8aca4d7c20e739e89b4b2cf1635dc7526
arkans <82564511+arkansys22@users.noreply.github.com>
2026-02-03 20:41:20 +0700

    up

--- application/views/potensial_clients/edit_pricelist.php
+++ application/views/potensial_clients/edit_pricelist.php
@@ -35,7 +35,7 @@
           <div class="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-9">
             <div class="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
               <h1 class="text-2xl font-bold mb-4">Edit Pricelist</h1>
-              <form action="<?= site_url('potensial-clients-pricelist/update_pricelist/'.$pc->data_pricelist_idsession) ?>" method="post" class="bg-white p-6 shadow-md rounded">
+              <form action="<?= site_url('potensial-clients-pricelist/update/'.$pc->data_pricelist_idsession) ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <label class="block mb-2">Judul</label>
                 <input type="text" name="judul" value="<?= $pc->data_pricelist_judul ?>" class="w-full px-4 py-2 border rounded mb-4" required>
 
