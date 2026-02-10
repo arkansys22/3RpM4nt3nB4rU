d6e5afdbbf5a9dab203b2d81c31e963293e835cf
arkans <82564511+arkansys22@users.noreply.github.com>
2026-02-10 15:45:06 +0700

    up

--- application/views/potensial_clients/view_penawaran.php
+++ application/views/potensial_clients/view_penawaran.php
@@ -246,10 +246,10 @@
             <p class="text-xs">Diskon berlaku hanya sampai H+5 setelah penawaran ini diberikan. Dan besaran diskon setiap harinya berkurang Rp 300.000.</p>
             <p class="text-xs">
                 Simulasi diskon contoh pertama : <br>
-                Pembayaran pertama pada H+3 setelah surat penawaran ini diberikan, maka kamu hanya dapat mengklaim diskon sebesar Rp 600.000.
+                Pembayaran pertama pada H+3 setelah surat penawaran ini diberikan, maka diskon kamu sudah berkurang sebesar Rp 600.000.
                 <br>
                 Simulasi diskon contoh kedua :<br>
-                Pembayaran pertama pada H+7 setelah surat penawaran ini diberikan, maka kamu tidak dapat mengklaim diskon sama sekali karena diskon sudah kadaluarsa.
+                Pembayaran pertama pada H+7 setelah surat penawaran ini diberikan, maka kamu tidak dapat mengklaim diskon sama sekali karena promo diskon sudah hangus.
             </p>
         </div>    
         <?php }?>
