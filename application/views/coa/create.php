1bd324884b7a61e62f624e28807fe2b95946bb7e
arkans <82564511+arkansys22@users.noreply.github.com>
2026-03-20 00:09:21 +0700

    up

--- application/views/coa/create.php
+++ application/views/coa/create.php
@@ -37,7 +37,7 @@
               <h2 class="text-2xl font-bold mb-4">Tambah Account</h2>
               <form action="<?= site_url('crews/store') ?>" method="post" class="bg-white p-6 shadow-md rounded">
                 <label class="block mb-2">Account Type</label>
-                <select name="account_type" id="account" class="w-full px-4 py-2 border rounded mb-4" required>
+                <select name="account_type" id="account_type" class="w-full px-4 py-2 border rounded mb-4" required>
                     <option value="">-- Pilih --</option>  
                     <option value="Cash/Bank">Cash/Bank</option>
                     <option value="Account Receivable">Account Receivable</option>
@@ -82,7 +82,7 @@
   <script>
     $(document).ready(function(){
 
-        $('#account').change(function(){
+        $('#account_type').change(function(){
 
             let type = $(this).val();
 
