18eaee3158036dc372002b83a2ebc4367e303ea7
arkans <82564511+arkansys22@users.noreply.github.com>
2026-03-29 09:43:34 +0700

    up

--- application/controllers/Crud_potensial_clients.php
+++ application/controllers/Crud_potensial_clients.php
@@ -1364,7 +1364,7 @@ class crud_potensial_clients extends CI_Controller {
 
             echo json_encode([
                 'status' => 'success',
-                'url' => base_url('assets/uploads/pricelist/' . $file['file_name'])
+                'url' => base_url('public_html/assets/uploads/pricelist/' . $file['file_name'])
             ]);
         }
     }
