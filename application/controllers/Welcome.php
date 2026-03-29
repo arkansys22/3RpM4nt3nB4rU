c10566359a887e72ab60d6938475b8d8c37e5cb2
arkans <82564511+arkansys22@users.noreply.github.com>
2026-03-29 17:34:22 +0700

    up

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,12 +18,12 @@ class Welcome extends CI_Controller {
 
 	public function pricelist()
 	{
-
+		$types = ['MC', 'WP dan WO', 'Dokumentasi', 'Catering', 'Dekorasi','Entertainment','Makeup & Busana']; 
 		$data['paketwo'] = $this->Potensial_model->get_pricelist_type('Paket Wedding Organizer');
 		$data['paketlamaran'] = $this->Potensial_model->get_pricelist_type('Paket Lamaran');
 		$data['paketgedung'] = $this->Potensial_model->get_pricelist_type('Paket Wedding Gedung');
 		$data['paketrumah'] = $this->Potensial_model->get_pricelist_type('Paket Wedding Rumah');
-		$data['paketvendor'] = $this->Potensial_model->get_pricelist_type('Dekorasi');
+		$data['paketvendor'] = $this->Potensial_model->get_pricelist_type($types);
 		$this->load->view('pricelist', $data);
 	}
 
