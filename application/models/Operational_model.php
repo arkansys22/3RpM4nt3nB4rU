4a18ab859d6a681e119580c6f9c3dff6ece72512
arkans <82564511+arkansys22@users.noreply.github.com>
2026-04-08 14:32:43 +0700

    up

--- application/models/Operational_model.php
+++ application/models/Operational_model.php
@@ -33,8 +33,26 @@ class Operational_model extends CI_Model {
     }
 
     public function insert_accounting($data_accounting) {
-        return $this->db->insert('accounting', $data_accounting);
 
+        // cek data yang sama (sesuaikan field uniknya)
+        $this->db->where('accounting_id_session', $data_accounting['accounting_id_session']);
+        $this->db->where('accounting_nomer_kategori', $data_accounting['accounting_nomer_kategori']);
+        $this->db->where('accounting_tanggal', $data_accounting['accounting_tanggal']);
+
+        $cek = $this->db->get('accounting');
+
+        if ($cek->num_rows() > 0) {
+            // UPDATE jika sudah ada
+            $this->db->where('accounting_id_session', $data_accounting['accounting_id_session']);
+            $this->db->where('accounting_nomer_kategori', $data_accounting['accounting_nomer_kategori']);
+            $this->db->where('accounting_tanggal', $data_accounting['accounting_tanggal']);
+
+            return $this->db->update('accounting', $data_accounting);
+
+        } else {
+            // INSERT jika belum ada
+            return $this->db->insert('accounting', $data_accounting);
+        }
     }
 
     public function update_accounting($id_session, $data_accounting) {
