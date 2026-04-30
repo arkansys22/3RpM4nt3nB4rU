3f881e5015f4f898768427ba73d458be88bf65ed
arkans <82564511+arkansys22@users.noreply.github.com>
2026-04-30 13:49:22 +0700

    up

--- application/views/project/crew_lihat.php
+++ application/views/project/crew_lihat.php
@@ -86,7 +86,7 @@
                         <a href="<?= $clients->list_photo ?>" target="_blank" class="w-full sm:w-auto block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-center">List Foto</a>
                       <?php endif; ?>
                       
-                        <a href=<?= site_url('naskah/data_pengantin/'.$project->id_session) ?>" target="_blank" class="w-full sm:w-auto block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-center">List Data Pengantin</a>
+                        <a href="<?= site_url('naskah/data_pengantin/'.$project->id_session) ?>" target="_blank" class="w-full sm:w-auto block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-center">List Data Pengantin</a>
                      
                     </div>
 
