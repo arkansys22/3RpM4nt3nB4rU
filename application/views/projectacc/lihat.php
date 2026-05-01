ad7437df6da31b470ec727a7c826e5978d67ec4d
arkans <82564511+arkansys22@users.noreply.github.com>
2026-05-01 17:05:44 +0700

    up

--- application/views/projectacc/lihat.php
+++ application/views/projectacc/lihat.php
@@ -246,7 +246,7 @@
 
                 <button onclick="showTab('aktual')" id="tab-aktual"
                   class="px-4 py-2 bg-gray-300 text-black rounded">
-                  Aktual
+                  Faktual
                 </button>
               </div>
 
