a4fbe74188b0afe44d6a2157773eca9a228946d8
arkans <82564511+arkansys22@users.noreply.github.com>
2026-05-05 16:33:11 +0700

    up

--- application/views/project/create.php
+++ application/views/project/create.php
@@ -155,6 +155,12 @@
         let date = selectedOption.getAttribute('data-date');
         let location = selectedOption.getAttribute('data-lokasi');
 
+        if (date) {
+            let parts = date.split('-'); // Y-m-d
+            date = parts[2] + '/' + parts[1] + '/' + parts[0];
+        }
+
+
         document.getElementById('client_name').value = name || '';
         document.getElementById('event_date').value = date || '';
         document.getElementById('lokasi').value = location || '';
