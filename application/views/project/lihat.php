d8debf20fb8c2e9e974a0679b2888f3d980b4f57
arkans <82564511+arkansys22@users.noreply.github.com>
2026-05-19 18:35:54 +0700

    up

--- application/views/project/lihat.php
+++ application/views/project/lihat.php
@@ -341,10 +341,12 @@
 
     <?php endif; ?>
 
+    <?php if (empty($agenda)): ?>
     <a href="<?= site_url('agenda/create/' . $project->id_session) ?>" 
        class="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 inline-block text-center">
        Tambah Agenda
     </a>
+    <?php endif; ?>
 
     </div>
 
